80c424ed543ebf37a9de7d82618cf449ff33cef9
linchuhang <linchuhang@yulintu.cn>
2024-04-26 11:46:30 +0800

    改变生成路径，修复分割地块bug

--- Component/YuLinTu.Component.MapFoundation/Topology/SplitItem.cs
+++ Component/YuLinTu.Component.MapFoundation/Topology/SplitItem.cs
@@ -5,22 +5,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using YuLinTu.tGIS.Client;
 
 namespace YuLinTu.Component.MapFoundation
 {
-    public class SplitItem
+    public class SplitItem : INotifyPropertyChanged
     {
+        #region Fields
+
+        private bool _isChecked;
+        private string _surveyNumber;
+
+        #endregion Fields
+
         #region Properties
 
         public Graphic Graphic { get; set; }
 
         public string Text { get; set; }
 
-        public string SurveyNumber { get; set; }
+        public string SurveyNumber
+        {
+            get { return _surveyNumber; }
+            set
+            {
+                if (_surveyNumber != value) // 只有当值发生变化时才触发事件
+                {
+                    _surveyNumber = value;
+                    OnPropertyChanged(nameof(SurveyNumber)); // 触发事件
+                }
+            }
+        }
+
+        public string OldNumber { get; set; }
+
+        public string NewNumber { get; set; }
 
         public Visibility Flag { get; set; }
 
+        // IsChecked 属性
+        public bool IsChecked
+        {
+            get { return _isChecked; }
+            set
+            {
+                if (_isChecked != value) // 只有当值发生变化时才触发事件
+                {
+                    _isChecked = value;
+                    OnPropertyChanged(nameof(IsChecked)); // 触发事件
+                }
+            }
+        }
+
+        public string DKMJ { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         #endregion Properties
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Component/YuLinTu.Component.Sender/Entrance.cs
+++ Component/YuLinTu.Component.Sender/Entrance.cs
@@ -5,15 +5,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
-using YuLinTu;
 using YuLinTu.Data;
 using YuLinTu.Library.Business;
 using YuLinTu.Library.Controls;
 using YuLinTu.Library.Entity;
-using YuLinTu.Library.Repository;
-using YuLinTu.Library.WorkStation;
 using YuLinTu.Windows;
 using YuLinTu.Windows.Wpf.Metro.Components;
 
@@ -24,27 +20,6 @@ namespace YuLinTu.Component.Sender
     /// </summary>
     public class Entrance : EntranceBase
     {
-        #region Properties
-
-        public ZoneDefine ZoneDefine { get; set; }
-
-        public List<LandVirtualPerson> ListPerson { get; set; }
-
-        public List<ContractLand> ContractLands { get; set; }
-        public List<ContractConcord> ContractConcords { get; set; }
-        public List<ContractRegeditBook> ContractRegeditBooks { get; set; }
-        public List<CollectivityTissue> Tissues { get; set; }
-
-        #endregion Properties
-
-        #region Fields
-
-        private ZoneDefine config;
-
-        private SettingsProfileCenter systemCenter;
-
-        #endregion Fields
-
         #region Methods
 
         /// <summary>
@@ -63,13 +38,6 @@ namespace YuLinTu.Component.Sender
         [MessageHandler(Name = ZoneMessage.ZONE_UPDATE_COMPLETE)]
         private void OnInstallZoneComplate(object sender, ModuleMsgArgs arg)
         {
-            IDbContext dbContext = arg.Datasource;
-            systemCenter = TheApp.Current.GetSystemSettingsProfileCenter();  //系统配置
-            var profile = systemCenter.GetProfile<ZoneDefine>();
-            var section = profile.GetSection<ZoneDefine>();
-            var tissueStation = dbContext.CreateCollectivityTissueWorkStation();
-            config = (section.Settings);
-            ZoneDefine = config.Clone() as ZoneDefine;
             IWorkpage page = sender as IWorkpage;
             MultiObjectArg multiObject = arg.Parameter as MultiObjectArg;
             if (page == null || multiObject == null)
@@ -87,128 +55,33 @@ namespace YuLinTu.Component.Sender
             GetZoneList(zdiOld as ZoneDataItem, oldZoneList);
             GetZoneList(zdiNew as ZoneDataItem, newZoneList);
             MultiObjectArg paraMeter = new MultiObjectArg() { ParameterA = newZoneList, ParameterB = oldZoneList };
-            if (ZoneDefine.SyncCode == true)
+            if (zdiNew.Name != zdiOld.Name)
             {
-                if (zdiNew.Name != zdiOld.Name)
+                //县级包括县以下的修改名称时需要提示陈泽林20161009
+                if (zdiNew.Level <= eZoneLevel.County)
                 {
-                    //县级包括县以下的修改名称时需要提示陈泽林20161009
-                    if (zdiNew.Level <= eZoneLevel.County)
+                    string content = "是否修改默认发包方名称?";
+                    TabMessageBoxDialog messagePage = new TabMessageBoxDialog()
                     {
-                        string content = "是否修改默认发包方名称?";
-                        TabMessageBoxDialog messagePage = new TabMessageBoxDialog()
+                        Header = SenderInfo.SenderEdit,
+                        Message = content,
+                        MessageGrade = eMessageGrade.Warn,
+                        CancelButtonText = "取消",
+                    };
+                    page.Page.ShowMessageBox(messagePage, (b, r) =>
+                    {
+                        if (zdiNew.FullCode != zdiOld.FullCode || (bool)b)
                         {
-                            Header = SenderInfo.SenderEdit,
-                            Message = content,
-                            MessageGrade = eMessageGrade.Warn,
-                            CancelButtonText = "取消",
-                        };
-                        page.Page.ShowMessageBox(messagePage, (b, r) =>
-                        {
-                            if (zdiNew.FullCode != zdiOld.FullCode || (bool)b)
-                            {
-                                paraMeter.ParameterC = (bool)b;
-                                SendUpMessage(sender, arg.Datasource, paraMeter);
-                            }
-                        });
-                    }
+                            paraMeter.ParameterC = (bool)b;
+                            SendUpMessage(sender, arg.Datasource, paraMeter);
+                        }
+                    });
                 }
-                else
-                {
-                    SendUpMessage(sender, arg.Datasource, paraMeter);
-                }
-            }
-            if (zdiNew.Level == eZoneLevel.Group)
-            {
-                Tissues = dbContext.CreateQuery<CollectivityTissue>().Where(x => x.ZoneCode == zdiOld.FullCode).ToList();
-                Tissues.ForEach(x => x.ZoneCode = zdiNew.FullCode);
-                ContractConcords = dbContext.CreateQuery<ContractConcord>().Where(x => x.ZoneCode == zdiOld.FullCode).ToList();
-                ContractConcords.ForEach(x => x.ZoneCode = zdiNew.FullCode);
-                ContractLands = dbContext.CreateQuery<ContractLand>().Where(x => x.LocationCode == zdiOld.FullCode).ToList();
-                ContractLands.ForEach(x =>
-                {
-                    x.LocationCode = zdiNew.FullCode;
-                    var Tissue = new CollectivityTissue();
-                    Tissue = tissueStation.Get(zdiNew.FullCode);
-                    x.ZoneCode = Tissue.Code;
-                });
-                ContractRegeditBooks = dbContext.CreateQuery<ContractRegeditBook>().Where(x => x.ZoneCode == zdiOld.FullCode).ToList();
-                ContractRegeditBooks.ForEach(x => x.ZoneCode = zdiNew.FullCode);
-                ListPerson = dbContext.CreateQuery<LandVirtualPerson>().Where(x => x.ZoneCode == zdiOld.FullCode).ToList();
-                ListPerson.ForEach(x => x.ZoneCode = zdiNew.FullCode);
-                UpTissues(dbContext);
-                UpContractConcord(dbContext);
-                UpRegeditBook(dbContext);
-                UpContractLand(dbContext);
-                UpVirtualPerson(dbContext);
             }
             else
             {
-                dbContext.ExecuteBySQL($"UPDATE JCSJ_FBF SET XZDYBM = REPLACE(XZDYBM, '{zdiOld.FullCode}', '{zdiNew.FullCode}') WHERE XZDYBM LIKE '%{zdiOld.FullCode}%';");
-
-                dbContext.ExecuteBySQL($"UPDATE ZD_CBD SET ZLDM = REPLACE(ZLDM, '{zdiOld.FullCode}', '{zdiNew.FullCode}') WHERE ZLDM LIKE '%{zdiOld.FullCode}%';");
-
-                dbContext.ExecuteBySQL($"UPDATE CBJYQ_HT SET DYBM = REPLACE(DYBM, '{zdiOld.FullCode}', '{zdiNew.FullCode}') WHERE DYBM LIKE '%{zdiOld.FullCode}%';");
-
-                dbContext.ExecuteBySQL($"UPDATE CBJYQ_QZ SET DYDM = REPLACE(DYDM, '{zdiOld.FullCode}', '{zdiNew.FullCode}') WHERE DYDM LIKE '%{zdiOld.FullCode}%';");
-
-                dbContext.ExecuteBySQL($"UPDATE QLR_CBF SET DYBM = REPLACE(DYBM, '{zdiOld.FullCode}', '{zdiNew.FullCode}') WHERE DYBM LIKE '%{zdiOld.FullCode}%';");
+                SendUpMessage(sender, arg.Datasource, paraMeter);
             }
-        }
-
-        private void UpVirtualPerson(IDbContext dbContext)
-        {
-            ContainerFactory factory = new ContainerFactory(dbContext);
-            var virtualPersonRep = factory.CreateRepository<IVirtualPersonRepository<LandVirtualPerson>>();
-            foreach (var entity in ListPerson)
-            {
-                virtualPersonRep.Update(entity);
-            }
-            virtualPersonRep.SaveChanges();
-        }
-
-        private void UpTissues(IDbContext dbContext)
-        {
-            ContainerFactory factory = new ContainerFactory(dbContext);
-            var tissueRep = factory.CreateRepository<ICollectivityTissueRepository>();
-            foreach (var entity in Tissues)
-            {
-                tissueRep.Update(entity);
-            }
-            tissueRep.SaveChanges();
-        }
-
-        private void UpContractConcord(IDbContext dbContext)
-        {
-            ContainerFactory factory = new ContainerFactory(dbContext);
-            var concordRep = factory.CreateRepository<IContractConcordRepository>();
-
-            foreach (var entity in ContractConcords)
-            {
-                concordRep.Update(entity);
-            }
-            concordRep.SaveChanges();
-        }
-
-        private void UpRegeditBook(IDbContext dbContext)
-        {
-            ContainerFactory factory = new ContainerFactory(dbContext);
-            var regeditBookRep = factory.CreateRepository<IContractRegeditBookRepository>();
-            foreach (var entity in ContractRegeditBooks)
-            {
-                regeditBookRep.Update(entity);
-            }
-            regeditBookRep.SaveChanges();
-        }
-
-        private void UpContractLand(IDbContext dbContext)
-        {
-            ContainerFactory factory = new ContainerFactory(dbContext);
-            var landRep = factory.CreateRepository<IContractLandRepository>();
-            foreach (var entity in ContractLands)
-            {
-                landRep.Update(entity);
-            }
-            landRep.SaveChanges();
         }
 
         /// <summary>
