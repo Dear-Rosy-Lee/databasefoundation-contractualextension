5315df0926f11ea07bdb009e41753387859a17f6
zhangjianmin <zjmin37@163.com>
2025-03-19 17:10:09 +0800

    .

---
+++ ComponentTask/YuLinTu.Component.VectorDataTreatTask/Task/VectorDataUpdateArgument.cs
@@ -0,0 +1,46 @@
+﻿using YuLinTu.Windows.Wpf.Metro.Components;
+
+namespace YuLinTu.Component.VectorDataTreatTask
+{
+    public class VectorDataUpdateArgument : TaskArgument
+    {
+        #region Fields
+
+        private string checkFilePath;
+        private string resultFilePath;
+
+        #endregion Fields
+
+        #region Properties
+
+        [DisplayLanguage("地块数据路径", IsLanguageName = false)]
+        [DescriptionLanguage("待处理地块数据文件的路径", IsLanguageName = false)]
+        [PropertyDescriptor(Builder = typeof(PropertyDescriptorBuilderFileBrowserShp),
+            UriImage16 = "pack://application:,,,/YuLinTu.Resources;component/Images/16/folder-horizontal-open.png")]
+        public string CheckFilePath
+        {
+            get { return checkFilePath; }
+            set
+            {
+                checkFilePath = value;
+                //qualityCompressionDataSetDefine.CheckFilePath = CheckFilePath;
+                NotifyPropertyChanged("CheckFilePath");
+            }
+        }
+
+        #endregion Properties
+
+        #region Ctor
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public VectorDataUpdateArgument()
+        {
+            checkFilePath = "";
+            resultFilePath = "";
+        }
+
+        #endregion Ctor
+    }
+}
---
+++ ComponentTask/YuLinTu.Component.VectorDataTreatTask/Task/VectorDataUpdateTask.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using CSScriptLibrary;
+using DotSpatial.Projections;
+using NetTopologySuite.Features;
+using Newtonsoft.Json;
+using Org.BouncyCastle.Tls;
+using YuLinTu.Appwork;
+using YuLinTu.Component.Account.Models;
+using YuLinTu.Library.Log;
+using YuLinTu.Windows;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace YuLinTu.Component.VectorDataTreatTask
+{
+    /// <summary>
+    /// 地块空间数据挂接任务
+    /// </summary>
+    [TaskDescriptor(IsLanguageName = false, Name = "地块矢量数据挂接",
+        UriImage16 = "pack://application:,,,/YuLinTu.Resources;component/Images/16/map--arrow.png",
+        UriImage24 = "pack://application:,,,/YuLinTu.Resources;component/Images/24/map.png")]
+    public class VectorDataUpdateTask : Task
+    {
+        #region Ctor
+
+        public VectorDataUpdateTask()
+        {
+            Name = "地块矢量数据挂接";
+            Description = "将矢量图斑按地块编码匹配更新到平台数据库中";
+        }
+
+        #endregion Ctor
+
+        #region Fields
+
+        private VectorDataUpdateArgument argument;
+
+        private int srid = 0;
+        private string prjstr;
+
+        #endregion Fields
+
+        #region Properties
+
+        private string ErrorInfo { get; set; }
+
+        #endregion Properties
+
+        #region Methods
+
+        #region Method - Override
+
+        /// <summary>
+        /// 开始执行任务
+        /// </summary>
+        protected override void OnGo()
+        {
+            this.ReportProgress(0, "开始验证检查数据参数...");
+            this.ReportInfomation("开始验证检查数据参数...");
+            System.Threading.Thread.Sleep(200);
+            if (!ValidateArgs())
+            {
+                this.ReportProgress(100);
+                return;
+            }
+
+            var token = AppGlobalSettings.Current.TryGetValue(Parameters.TokeName, "");
+            if (string.IsNullOrEmpty(token))
+            {
+                this.ReportError("请先登录后,再次进行操作");
+                return;
+            }
+            this.ReportProgress(1, "开始更新...");
+            this.ReportInfomation("开始更新...");
+            System.Threading.Thread.Sleep(200);
+
+            string zonecode = AppGlobalSettings.Current.TryGetValue(Parameters.RegionName, "");// Parameters.Region.ToString();
+            string baseUrl = TheApp.Current.GetSystemSection().TryGetValue("DefaultSystemService", AppParameters.stringDefaultSystemServiceValue);
+            string postGetTaskIdUrl = $"/ruraland/api/topology/update/shape/{zonecode}/false/by/dkbm";
+            var sourceFolder = argument.CheckFilePath.Substring(0, argument.CheckFilePath.Length - 4);
+            //进行质检 
+            if (DataCheck())
+            {
+                try
+                {
+                    ProcessDataOnline(baseUrl, postGetTaskIdUrl, token);
+                    this.ReportProgress(100);
+                    this.ReportInfomation("数据处理成功。");
+                    //CompressAndEncryptFolder(sourceFolderPath, zipFilePath, password);
+                }
+                catch (Exception ex)
+                {
+                    Log.WriteException(this, "OnGo(数据处理失败!)", ex.Message + ex.StackTrace);
+                    this.ReportError(string.Format("数据处理出错!" + ex.Message));
+                }
+            }
+        }
+
+        #endregion Method - Override
+
+        #region Method - Private
+
+        /// <summary>
+        /// 参数验证
+        /// </summary>
+        /// <returns></returns>
+        private bool ValidateArgs()
+        {
+            var args = Argument as VectorDataUpdateArgument;
+
+            if (args == null)
+            {
+                this.ReportError(string.Format("参数错误!"));
+                return false;
+            }
+            argument = args;
+            try
+            {
+                if (args.CheckFilePath.IsNullOrEmpty())
+                {
+                    this.ReportError(string.Format("待处理数据文件路径为空，请重新选择"));
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                YuLinTu.Library.Log.Log.WriteException(this, "ValidateArgs(参数合法性检查失败!)", ex.Message + ex.StackTrace);
+                return false;
+            }
+            this.ReportInfomation(string.Format("检查数据参数正确。"));
+            return true;
+        }
+
+        /// <summary>
+        /// 数据检查
+        /// </summary>
+        /// <returns></returns>
+        private bool DataCheck()
+        {
+            try
+            {
+                ErrorInfo = "";
+                string filepath = Path.GetDirectoryName(argument.CheckFilePath);
+                string filename = Path.GetFileNameWithoutExtension(argument.CheckFilePath);
+                if (!CheckFile(filepath, filename))
+                    return false;
+                var sr = VectorDataProgress.GetByFile(filepath + "\\" + filename);
+                srid = sr.WKID;
+                prjstr = sr.WKT;
+                if (srid == 0)
+                {
+                    this.ReportError("未正确获取到矢量文件的srid,请修正数据后再此执行！");
+                    return false;
+                }
+                //CompressAndEncryptFolder(sourceFolderPath, zipFilePath, password);
+            }
+            catch (Exception ex)
+            {
+                Library.Log.Log.WriteException(this, "OnGo(数据检查失败!)", ex.Message + ex.StackTrace);
+                this.ReportError(string.Format("数据检查时出错!"));
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 检查必要文件
+        /// </summary>
+        /// <param name="filepath"></param>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public bool CheckFile(string filepath, string filename)
+        {
+            // 校验文件是否存在
+            string[] requiredExtensions = { ".dbf", ".prj", ".shp", ".shx" };
+
+            foreach (string extension in requiredExtensions)
+            {
+                string fullPath = Path.Combine(filepath, filename + extension);
+                if (!File.Exists(fullPath))
+                {
+                    ErrorInfo = $"缺少文件: {filename + extension}";
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 在线处理数据
+        /// </summary>
+        /// <returns></returns>
+        public void ProcessDataOnline(string url, string murl, string key)
+        {
+            int srid = 0;
+            var landprj = Path.ChangeExtension(argument.CheckFilePath, "prj");
+            using (var sreader = new StreamReader(landprj))
+            {
+                if (string.IsNullOrEmpty(prjstr))
+                    prjstr = sreader.ReadToEnd();
+                //int.TryParse(GetMarkValue(prjstr, "EPSG", 6), out srid);
+            }
+            var landShapeList = VectorDataProgress.InitiallShapeLandList(argument.CheckFilePath, srid, "");
+            if (landShapeList == null)
+            {
+                return;
+            }
+
+            string prj4490 = "GEOGCS[\"GCS_China_Geodetic_Coordinate_System_2000\",DATUM[\"D_China_2000\",SPHEROID[\"CGCS2000\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]]";
+            ProjectionInfo sreproject = ProjectionInfo.FromEsriString(prj4490);
+            ProjectionInfo dreproject = ProjectionInfo.FromEsriString(prjstr);
+            this.ReportProgress(5, "开始处理数据");
+            var p = 90.0 / landShapeList.Count();
+            int pindex = 1;
+            List<LandEntity> ls = new List<LandEntity>();
+            foreach (var item in landShapeList)
+            {
+                var land = new LandEntity();
+                land.dkbm = item.DKBM;
+                land.qqdkbm = item.QQDKBM;
+                var ygeo = item.Shape as YuLinTu.Spatial.Geometry;
+                ygeo = VectorDataProgress.ReprojectShape(ygeo, dreproject, sreproject, 4490);
+                land.ewkt = $"SRID=4490;{ygeo.GeometryText}";
+                ls.Add(land);
+            }
+
+            var re = DataProcessOnLine(url, murl, ls, key);
+            if (re != "200")
+            {
+                this.ReportInfomation(re);
+            }
+        }
+
+        /// <summary>
+        /// 在线处理数据
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private string DataProcessOnLine(string baseUrl, string murl, List<LandEntity> data, string token)
+        {
+            ApiCaller apiCaller = new ApiCaller();
+            apiCaller.client = new HttpClient();
+            string jsonData = JsonConvert.SerializeObject(data);
+            try
+            {
+                var getTaskID = apiCaller.PostGetTaskIDAsync(token, $"{baseUrl}{murl}", jsonData);
+                string postGetResult = $"{baseUrl}/ruraland/api/tasks/schedule/job";
+                var getResult = apiCaller.PostGetResultAsync(token, postGetResult, getTaskID);
+                //var getresult = apiCaller.PostDataAsync(url, jsonData, token);
+                return ConstructCoordsFromString(getResult);
+            }
+            catch (Exception ex)
+            {
+                ErrorInfo = "图形处理发生错误,详情请查看检查结果";
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 通过点坐标文件构造图形
+        /// </summary>
+        private string ConstructCoordsFromString(KeyValueList<string, string> pointstr)
+        {
+            if (pointstr == null)
+            {
+                return "更新图斑时服务器出现问题";
+            }
+            if (pointstr.Count > 0)
+            {
+                return $"成功更新{pointstr.GetValue("results") as string}条数据";
+            }
+            return "";
+        }
+
+        #endregion Method - Private
+
+        #endregion Methods
+    }
+
+}
