3b7f033dc9bbb04173c43f06ca17798b614a306c
zhangjianmin <zjmin37@163.com>
2025-05-25 10:51:41 +0800

    调整程序生成目录结构

--- Worker/AppEntrance.cs
+++ Worker/AppEntrance.cs
@@ -12,57 +12,17 @@ namespace YuLinTu.Product.YuLinTuTool
         [STAThread]
         private static void Main(string[] args)
         {
-            //string progressName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
-            //int produceNumber = 0;
-            //foreach (System.Diagnostics.Process progress in System.Diagnostics.Process.GetProcesses())
-            //{
-            //    if (progress.ProcessName != progressName)
-            //    {
-            //        continue;
-            //    }
-            //    produceNumber++;
-            //}
-            //if (produceNumber > 1)
-            //{
-            //    MessageBox.Show("软件已经在运行!", "运行提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            //    Application.Exit();
-            //    return;
-            //}
-            //AppShellWpf shell = new AppShellWpf();
-            //shell.Run(args);
-
-            //GC.Collect();
-            //GC.WaitForPendingFinalizers();
-            //GC.Collect();
-            //GC.WaitForPendingFinalizers();
-
-            //Data.SQLite.ProviderDbCSQLite.ShutdownAllConnection();
-            /*
-             var builder = new ContainerBuilder();
-             var assemblyFiles = Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Libraries", "libs"), "YuLinTu.Library.*.dll");
-             var assemblies = new List<Assembly>();
-
-             foreach (var file in assemblyFiles)
-             {
-                 var assembly = Assembly.LoadFile(file);
-                 assemblies.Add(Assembly.LoadFrom(file));
-                 builder.Register(assembly);
-             }
-
-             // 注册工作单元拦截器
-             builder.RegisterInstance(new UnitOfWorkInterceptor());
-
-             ContainerProvider.Load(builder);
-
-             builder.RegisterBuildCallback(container =>
-             {
-                 //注册 IValidator
-                 //container.RegisterValidators(assemblies);
-             });
-
-             // 注册 GDAL
-             //GdalShapefile.Register();
-            */
+            // 设置环境变量
+            string variableName = "Path";
+            // 获取环境变量
+            string value = Environment.GetEnvironmentVariable(variableName);
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Libraries\libs");
+            string sppath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Libraries\x86");
+            if (!value.Contains(path))
+            {
+                value = value + ";" + path + ";" + sppath;
+                Environment.SetEnvironmentVariable(variableName, value);
+            }
             AppShellWpf shell = new AppShellWpf();
             shell.Run(args);
         }
