90f5f1337b6da4af99d170b090ff6edef1ed82af
suchuan <2802955891@qq.com>
2025-06-23 18:02:43 +0800

    .

--- Component/YuLinTu.Component.VectorDataDecoding/Core/Constants.cs
+++ Component/YuLinTu.Component.VectorDataDecoding/Core/Constants.cs
@@ -13,13 +13,13 @@ namespace YuLinTu.Component.VectorDataDecoding.Core
         internal static string baseUrl = ConfigurationManager.AppSettings.TryGetValue<string>("DefaultBusinessAPIAdress", "https://api.yizhangtu.com");
         internal const string appidName = "t-open-api-app-id";
 
-        internal const string appidVaule = "2d954f1f39d04ea5be07d7b67d6c1ad7";
+        internal const string appidVaule = "8cebb535fc5f40aaa56290faa43d8648";
         internal const string moduleUniqueKey = "9A75153E-E8E3-4219-A3D3-3670CAB204E9";
 
         internal const string appKeyName = "t-open-api-app-key";
         //以下配置应登录账号后获取，目前先硬编码写死
 
-        internal const string appKeyVaule = "2tlb6nii0VYncW6Gq/AKDWSO8JU9Kj39e64syJUfy3tMVVUmaYqjYA==";
+        internal const string appKeyVaule = "9bdq7mR4BkaRVmU3JKgG38ISz9D+VY7optuGREIIA3gXDgCi9jyaTQ==";
 
         internal const string Sm4Key = "efd4a17e7c2a89ea5a9d9a283fe03ae1";//"efd4a17e7c2a89ea";//5a9d9a283fe03ae1";
         //internal const int Sm4Key_16 = #efd4a17e7c2a89ea5a9d9a283fe03ae1;
