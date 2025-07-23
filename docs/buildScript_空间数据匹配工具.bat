4000d50cc519d2cdbf17546a72415deb471eb91f
suchuan <2802955891@qq.com>
2025-07-23 14:45:29 +0800

    .

--- docs/buildScript_空间数据匹配工具.bat
+++ docs/buildScript_空间数据匹配工具.bat
@@ -1,2 +1,2 @@
-"D:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc "D:\Program Files (x86)\Jenkins\jobs\图斑匹配工具\WorkSpace\docs\buildScript_空间数据匹配工具.iss"
+"D:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc "D:\Program Files (x86)\Jenkins\jobs\空间数据匹配工具\WorkSpace\docs\buildScript_空间数据匹配工具.iss"
 pause
