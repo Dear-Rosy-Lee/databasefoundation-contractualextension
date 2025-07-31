2ed5cd3b9d78198bf4abaa1a80e2f19125754611
suchuan <2802955891@qq.com>
2025-07-31 17:00:53 +0800

    图斑接入工具

--- Worker/Properties/AssemblyInfo.cs
+++ Worker/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SpatitalDataEncryptTool")]
+[assembly: AssemblyTitle("SptialDataIntegrationTool")]
 [assembly: AssemblyDescription("")]
-[assembly: Guid("8A75153E-E8E3-4219-A3D3-3670CAB204E8")]
+[assembly: Guid("8A76163E-E8E3-4219-A3D3-3670CAB204E8")]
