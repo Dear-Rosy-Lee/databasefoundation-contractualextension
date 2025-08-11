fef13e1c9423a5229c80aca64f268c8bba7eb844
zhangjianmin <zjmin37@163.com>
2025-08-11 15:37:39 +0800

    .

---
+++ Library/YuLinTu.Library.Entity/Enum/eQRType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuLinTu.Library.Entity
+{
+    /// <summary>
+    /// 生成二维码类型
+    /// </summary>
+    public enum eQRType
+    {
+        /// <summary>
+        /// 网址二维码
+        /// </summary>
+        UrlQR = 1,
+
+        /// <summary>
+        /// 自定义二维码
+        /// </summary>
+        CustomQR = 2
+    }
+}
